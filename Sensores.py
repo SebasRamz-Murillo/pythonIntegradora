fbd3477c693af544653653297043781290182d89
Sebastian Ramirez <71026252+SebasRamz-Murillo@users.noreply.github.com>
2023-04-21 13:02:59 -0600

    wuuu

--- Sensores.py
+++ Sensores.py
@@ -2,12 +2,15 @@ from Sensor import Sensor
 from UltimaLectura import ultimaLectura
 from Lista import Lista
 import random
+# import RPi.GPIO as GPIO
 from gpiozero import LED
 from datetime import datetime, timedelta
 from Api import Api
 import time
 import serial
 import json
+
+
 class Sensores:
     def __init__(self):
         self.sensores = Sensor()
@@ -20,73 +23,84 @@ class Sensores:
         self.ledPost = LED(13)
         self.ledWInternet = LED(21)
         print("Configurando canal: ", self.ledInternet)
+        # GPIO.setup(self.ledInternet, GPIO.OUT)
 
-    def lecturaSerial(self):
-        ser = serial.Serial(self.puerto, self.baudios, timeout=1)
-        ser.reset_input_buffer()
-        while True:
-            if ser.in_waiting > 0:
-                line = ser.readline().decode('utf-8').rstrip()
-                return line
-    
+    # def lecturaSerial(self):
+    #     ser = serial.Serial(self.puerto, self.baudios, timeout=1)
+    #     ser.reset_input_buffer()
+    #     while True:
+    #         if ser.in_waiting > 0:
+    #             line = ser.readline().decode('utf-8').rstrip()
+    #             return line
+
     def guardarDatos(self, data, redStatus, apiStatus):
         newSensor = Sensor(data)
         if not redStatus or not apiStatus:
             self.historico.agregar(newSensor.to_dict())
             print("guardando en historico")
         else:
-            historial=self.historico.mostrar()
+            historial = self.historico.mostrar()
             if len(historial) > 0:
-                x=0
+                x = 0
                 for sens in historial:
                     file = sens["clave"] + ".json"
                     Lista(file).agregar(sens)
                     self.historico.eliminar(x)
-                    x+=1
+                    x += 1
             file = data["clave"] + ".json"
             Lista(file).agregar(newSensor.to_dict())
-            #aqui va la insercion a mongo desde la api
-            sensor= newSensor.to_dict()
-            dataApi={
-                "clave":sensor["clave"],
-                "tipo":sensor["tipo"],
-                "valores":sensor["valores"],
-                "dato":sensor["dato"],
-                "fecha":sensor["fecha"],
-                "hora":sensor["hora"],
-                "pines":sensor["pines"],
-                "file":sensor["file"]
+            # aqui va la insercion a mongo desde la api
+            sensor = newSensor.to_dict()
+            dataApi = {
+                "clave": sensor["clave"],
+                "tipo": sensor["tipo"],
+                "valores": sensor["valores"],
+                "dato": sensor["dato"],
+                "fecha": sensor["fecha"],
+                "hora": sensor["hora"],
+                "pines": sensor["pines"],
+                "file": sensor["file"]
             }
             print(dataApi)
             if not self.api.post_data(newSensor.to_dict()):
                 self.historico.agregar(newSensor.to_dict())
-            #aaaaaaaaaaaaaaaaaa
+            # aaaaaaaaaaaaaaaaaa
             horaPrincipal = Lista(file).mostrar()[0]["hora"]
             horaTurno = Lista(file).mostrar()[len(Lista(file).mostrar()) - 1]["hora"]
             if (datetime.strptime(horaTurno, "%H:%M:%S") - timedelta(minutes=5) >= datetime.strptime(horaPrincipal,
                                                                                                      "%H:%M:%S")) or len(
-                    Lista(file).mostrar()) > 200:
+                Lista(file).mostrar()) > 200:
                 Lista(file).borrarInfo()
 
-    def ledOn(self,led):
+    def ledOn(self, led):
         print("Sucedio algooooooooo aaaaaaaaaaaa")
+        # GPIO.output(led, GPIO.HIGH)
         led.on()
-    def ledOff(self,led):
+
+    def ledOff(self, led):
         print("SE APAGOOOOO aaaaaaaaaaaa")
+        # GPIO.output(led, GPIO.LOW)
         led.off()
 
-
-
     def checkarHistorico(self):
-        dataHistorial=self.historico.to_dict()
+        dataHistorial = self.historico.to_dict()
         return dataHistorial
-    
 
-    
 
 if __name__ == "__main__":
     while True:
+        # for i in range(5):
         valorRandom = random.randint(2, 60)
+        # asi debe quedar el json que se reciba o al menos tener esos datos desde el arduino, menos dispositivo
+        # data = {
+        #    "clave": "Ult2",
+        #    "tipo": "temperatura",
+        #    "valores": valorRandom,
+        #    "dato": "C",
+        #    "pines": "2,3",
+        #    "dispositivo": "carrito1"
+        # }
+
         sens = Sensores()
         Communication = serial.Serial(sens.puerto, sens.baudios)
         data = Communication.readline().decode().strip()
@@ -110,7 +124,6 @@ if __name__ == "__main__":
         else:
             sens.ledOn(sens.ledWInternet)
             time.sleep(10)
-
 
 
 
